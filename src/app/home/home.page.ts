90e9598386c5788016794c51c93cf87e5fc8da20
gu12ga <gustavogabrielmartins55@gmail.com>
2023-09-24 17:44:39 -0300

    calcula imc e dura 3 segudos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+
+  weigth!: number
+  heigth!: number
+
+  constructor(private toastController: ToastController) {}
+
+  onCalculate(){
+
+    const imc = this.weigth/(this.heigth*this.heigth);
+    this.showMessage(`IMC: ${imc.toFixed(2)}`)
+
+  }
+
+  async showMessage(msg: string){
+
+    const toast = await this.toastController.create(
+      {
+        message: msg,
+        duration: 3000
+
+      }
+    );
+
+    toast.present();
+  }
 
 }
